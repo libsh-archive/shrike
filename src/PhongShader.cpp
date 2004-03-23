158842b7e56080c580c4a4dfe1b24eec7d3cb58a
sjdutoit <sjdutoit@afdca40c-03d6-0310-8ede-e9f093b21075>
2004-03-23 00:29:43 +0000

    add textures to phong
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/shrike/trunk@559 afdca40c-03d6-0310-8ede-e9f093b21075

--- src/PhongShader.cpp
+++ src/PhongShader.cpp
@@ -33,14 +33,25 @@ bool PhongShader::init()
 {
   vsh = ShKernelLib::shVsh( Globals::mv, Globals::mvp );
   vsh = vsh << shExtract("lightPos") << Globals::lightPos; 
-  vsh = shSwizzle("normal","halfVec", "lightVec", "posh") << vsh;
+  vsh = shSwizzle("normal","halfVec", "lightVec", "posh", "texcoord") << vsh;
 
   ShColor3f SH_DECL(specular) = ShColor3f(0.5, 1.0, 1.0);
   ShColor3f SH_DECL(diffuse) = ShColor3f(1.0, 0.0, 0.0);
   ShAttrib1f SH_DECL(exponent) = ShAttrib1f(35.0);
   exponent.range(5.0f, 500.0f);
+
+  ShImage image;
+  image.loadPng(SHMEDIA_DIR "/textures/rustkd.png");
+  ShTexture2D<ShColor3f> difftex(image.width(), image.height());
+  difftex.memory(image.memory());
+  image.loadPng(SHMEDIA_DIR "/textures/rustks.png");
+  ShTexture2D<ShColor3f> spectex(image.width(), image.height());
+  spectex.memory(image.memory());
   
-  fsh = ShKernelLib::shPhong<ShColor3f>() << diffuse << specular << exponent; 
+  fsh = ShKernelLib::shPhong<ShColor3f>()
+    << (access(difftex) & access(spectex) & (keep<ShAttrib1f>() << exponent))
+    << (shDup() << (shExtract("texcoord")
+                    << ShKernelLib::outputPass(vsh)));
   return true;
 }
 
