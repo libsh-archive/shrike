d2b6af1d6776846e1f3ba441dfac4f8d0a34e38c
sjdutoit <sjdutoit@afdca40c-03d6-0310-8ede-e9f093b21075>
2004-06-11 14:20:46 +0000

    Many improvements to the LCD shader
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/shrike/trunk@1037 afdca40c-03d6-0310-8ede-e9f093b21075

--- src/shaders/LCD.cpp
+++ src/shaders/LCD.cpp
@@ -47,14 +47,13 @@ ShAttrib<N, SH_CONST> construct(double a, ...)
   return ShAttrib<N, SH_CONST>(args);
 }
 
-ShAttrib1f lcd(const ShTexCoord2f& tc,
-               ShAttrib1f number,
-               int digits = 3,
-               bool showgrid = false)
+ShAttrib1f lcd(const ShTexCoord2f& tc, ShAttrib1f number,
+               int digits = 3, bool showgrid = false, bool handleneg = true)
 {
   float w = 0.2;
   float h = 0.5;
   float t = 0.02;
+  float eps = 0.01;
 
   ShAttrib<7, SH_CONST> segments[10] = {
     //           TT   LT   RT   CE   LB   RB   BB  
@@ -70,40 +69,55 @@ ShAttrib1f lcd(const ShTexCoord2f& tc,
     construct<7>(1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0)};// 9
   
   ShAttrib<7, SH_CONST> posns[4] = {
-    construct<7>(0.0  , 0.0  , w - t, t          , 0.0  , w - t  , 0.0), // left
-    construct<7>(w    , t    , w    , w - t      , t    , w      , w  ), // right
-    construct<7>(h - t, h/2.0, h/2.0, (h - t)/2.0, 0.0  , 0.0    , 0.0), // bottom
-    construct<7>(h    , h    , h    , (h + t)/2.0, h/2.0, h/2.0  , t  )};  // top
-  
-  float eps = 0.01;
+    construct<7>(0.0  , 0.0  , w - t, t          , 0.0  , w - t  , 0.0),  // left
+    construct<7>(w    , t    , w    , w - t      , t    , w      , w  ),  // right
+    construct<7>(h - t, h/2.0, h/2.0, (h - t)/2.0, 0.0  , 0.0    , 0.0),  // bottom
+    construct<7>(h    , h    , h    , (h + t)/2.0, h/2.0, h/2.0  , t  )}; // top
   
   ShAttrib1f result(0.0f);
+
   ShTexCoord2f loc = tc;
+  ShAttrib1f f = floor(loc(0) / (w + t));
+  ShAttrib1f index = f - digits + 1;
+  
+  number = number * pow(10.0, index);
 
-  loc(0) -= (w + t)*digits;
-  for (int i = 0; i < digits; i++) {
-    ShAttrib1f digit = floor(number) - floor(number / 10.0f)*10.0f;
-    
-    ShAttrib<7, SH_TEMP> in[4];
+  loc(0) = loc(0) - f * (w+t);
+  ShAttrib1f digit = floor(number) - floor(number / 10.0f)*10.0f;
 
-    in[0] = posns[0] < loc(0);
-    in[1] = posns[1] > loc(0);
-    in[2] = posns[2] < loc(1);
-    in[3] = posns[3] > loc(1);
-
-    in[0] = in[0] * in[1] * in[2] * in[3];
-
-    for (int d = 0; d < 10; d++) {
-      result += (abs(digit - (float)d) < eps) * dot(segments[d], in[0]);
-    }
-    
-    if (showgrid) {
-      result += (dot(fillcast<7>(ShConstAttrib1f(0.1)), in[0]) > 0.0) * 0.2;
-    }
-
-    loc(0) += w + t;
-    number /= 10.0f;
+  if (handleneg) {
+    digit = cond(number < 0.0, 9.0 - digit, digit);
   }
+
+  // ShAttrib1f digit = abs(index); // Useful for debugging
+  
+  ShAttrib<7, SH_TEMP> in[4];
+  
+  in[0] = posns[0] < loc(0);
+  in[1] = posns[1] > loc(0);
+  in[2] = posns[2] < loc(1);
+  in[3] = posns[3] > loc(1);
+  
+  in[0] = in[0] * in[1] * in[2] * in[3];
+  
+  for (int d = 0; d < 10; d++) {
+    result += (abs(digit - (float)d) < eps) * dot(segments[d], in[0]);
+  }
+  
+  if (handleneg) {
+    result = cond(index < -0.9 && abs(number/10.0) < 0.1,
+                  ShConstAttrib1f(0.0f), result);
+    result = cond(number < 0.0 && index < -0.9 && abs(number/10.0) < 0.1 && abs(number) > 0.1,
+                  in[0][3], result);
+    result = cond(number < 0.0 && index > -1.1 && index < -0.9 && abs(number) < 0.1,
+                  in[0][3], result);
+  }
+
+  if (showgrid) {
+    result = result || (dot(fillcast<7>(ShConstAttrib1f(0.1)), in[0]) > 0.0) * 0.2;
+  }
+
+  
   return result;
 }
 
@@ -126,12 +140,14 @@ bool LCD::init()
   } SH_END;
 
   ShAttrib1f SH_DECL(value);
-  value.range(0.0, 999.9);
+  value.range(-500.1, 50.1);
 
   ShColor3f SH_DECL(background) = ShColor3f(0.69, 0.75, 0.68);
   ShColor3f SH_DECL(foreground) = ShColor3f(0.18, 0.20, 0.18);
 
   ShVector2f SH_DECL(offset) = ShVector2f(0.0, 0.25);
+  ShVector2f SH_DECL(scale) = ShVector2f(1.0, 1.0);
+  scale.range(0.01, 3.0);
 
   fsh = SH_BEGIN_PROGRAM("gpu:fragment") {
     ShInputNormal3f normal;
@@ -144,6 +160,7 @@ bool LCD::init()
     normal = normalize(normal);
     light = normalize(light);
 
+    tc *= scale;
     tc(1) = 1.0 - tc(1);
     result = cond(lcd(tc - offset, value), foreground, background);
   } SH_END;
