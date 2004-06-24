7d4d98c01401a11db04f4e5a82f571aa714dd5b2
sfvvasse <sfvvasse@afdca40c-03d6-0310-8ede-e9f093b21075>
2004-06-24 21:10:11 +0000

    New version of the 1D non peridodic tilings
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/shrike/trunk@1083 afdca40c-03d6-0310-8ede-e9f093b21075

--- src/shaders/NonPeriodic.cpp
+++ src/shaders/NonPeriodic.cpp
@@ -31,45 +31,18 @@ NonPeriodic::~NonPeriodic()
 {
 }
 
-ShAttrib1f do_mod(ShAttrib1f x)
+static const double m = 0.5*(sqrt( 5.0 ) + 1.0);
+static const double m2 = 0.5*(sqrt( 5.0) - 1.0);
+
+ShAttrib1f isRed(ShAttrib1f x, ShMatrix3x3f M)
 {
-  return frac(x) - 0.5;
-}
+  ShAttrib1f mx = frac( x );
+  ShAttrib1f my = frac( m2*x );
 
-ShAttrib1f aboveLine( ShAttrib1f x0, ShAttrib1f y0, ShAttrib1f m, ShAttrib1f x, ShAttrib1f y )
-{
-  return (y >= (m*(x-x0) + y0));
-}
-
-static const double m = 0.5*(sqrt( 5.0 ) - 1.0);
-static const double pm = -1.0 / m;
-
-ShAttrib1f isRed(ShAttrib1f x)
-{
-  ShAttrib1f mx = do_mod( x );
-  ShAttrib1f my = do_mod( m*x - 0.25*sqrt(5.0) );
-
-  // The following conditions amount to checking whether the
-  // point (mx,my) lies in one of two polygons inside the
-  // unit square.  Each region is actually the intersection of
-  // the unit square with a smaller square that partially overlaps
-  // it.  That should make for fairly nice tests later if we want.
-
-  // Basically use these four lines to define a tic-tac-toe
-  // board, use 3*tb+lr to tell what cell you're in.
-
-  ShAttrib1f lr = 0;
-  ShAttrib1f tb = 0;
-
-  lr = cond(aboveLine(-0.5, 1.5, pm, mx, my ), ShConstAttrib1f(2.0),
-            cond(aboveLine( -0.5, 0.5, pm, mx, my ), ShConstAttrib1f(1.0), ShConstAttrib1f(0.0)));
-
-  tb = cond(aboveLine( 0.5, 0.5, m, mx, my ), ShConstAttrib1f(2.0),
-            cond(aboveLine( -0.5, -0.5, m, mx, my ), ShConstAttrib1f(1.0), ShConstAttrib1f(0.0)));
-
-  ShAttrib1f qq = tb*3.0 + lr;
-
-  return abs(qq) < 0.1 || abs(qq - 7) < 0.1;
+	ShVector3f v = ShVector3f(mx,my,ShAttrib1f(1.0));
+	v = M | v;
+	
+  return max(abs(v(0)),abs(v(1)))<0.5;
 }
 
 bool NonPeriodic::init()
@@ -92,6 +65,12 @@ bool NonPeriodic::init()
   ShAttrib1f SH_DECL(second) = 0.0;
   second.range(0.0, 1.0);
 
+	ShMatrix3x3f M = ShMatrix3x3f();
+	M[0][1]=-m;
+	M[1][0]=m;
+	M[0][2]=0.5*(m-1.0);
+	M[1][2]=-0.5*(m+1.0);
+	
   // This is a 1D nonperiodic tiling from Craig Kaplan.
   // It should be possible to extend this to make Penrose tiles.
   fsh = SH_BEGIN_PROGRAM("gpu:fragment") {
@@ -100,9 +79,8 @@ bool NonPeriodic::init()
     ShOutputColor3f result;
 
     tc *= scale;
-    
-    result = cond(isRed(tc(0)) || (second && isRed(tc(1))),
-                  color1, color2);
+    result = cond(isRed(tc(0),M) || (second && isRed(tc(1),M)),
+										color1, color2);
   } SH_END;
   return true;
 }
