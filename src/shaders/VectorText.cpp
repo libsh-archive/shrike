643a74a9b8b3ac05bd9757b8594b29dbd4efb7a3
mmccool <mmccool@afdca40c-03d6-0310-8ede-e9f093b21075>
2005-03-08 03:18:19 +0000

    Cleaned up some things in the vector text demo: better defaults for parameters, so letter is centered at startup.   Changed colour in distance map to avoid red-green blindness issues and scaled things better.   Added computation of gradient and gradient visualization.   Added anisotropic filtering (which looks really, really nice, especially on curved surfaces. When isotropically filtered letters get distorted and stretched they tend to blur; the anisotropic filtering mode corrects for that, and happens to be relatively cheap).
    Added modes for outline fonts, both isotropically and anisotropically
    antialiased.
    
    Debated checking in, since that open sources it... but oh, what the heck.
    However, more efficient data representations and encapsulations should be 
    kept in closed-source form from now on.   Need to branch separate project
    in closed repository to contnue this work.
    
    Probably should put segdist function in standard library.   Noted that
    smoothpulse function is missing in standard library (I think... not 
    documented in doxygen, anyways).
    
    
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/shrike/trunk@2107 afdca40c-03d6-0310-8ede-e9f093b21075

--- src/shaders/VectorText.cpp
+++ src/shaders/VectorText.cpp
@@ -49,53 +49,55 @@ private:
 
 /* SegDist
  *
- * Compute distance from a point to a line segment.
+ * Compute distance and gradient from a point to a line segment.
  *
- * This functions returns the squared distance, the true
- * distance, and a value whose sign lets us know which side of the edge
- * we are on.  These are packed into the zeroth, first, and second 
- * components of a tuple.   We depend on dead-code elimination (which
- * hopefully will be extended to work on components of tuples) to get
- * rid of computations that are not needed.   
+ * This functions returns the unsigned distance, an evaluation of the
+ * plane equation (whose sign lets us know which side of the edge
+ * we are on), and the gradient of the distance to a line segment.  
+ * These are packed into the components of a 4-tuple.   
+ *
+ * We depend on dead-code elimination (which is supposed to work 
+ * on components of tuples) to get rid of computations that are not 
+ * needed.   
  */
-ShAttrib3f //< squared distance, true distance, and signed value
+ShAttrib4f //< true distance, signed value (dist from line), gradient vector
 segdist (
     ShAttrib4f L,  //< line segment (0,1 point 0, 2,3 point 1)
     ShPoint2f  x   //< test point
 ) {
+    // return value
+    ShAttrib4f r;
+
     // compute 2D vectors from first endpoint to x (delta(0,1)) and
     // from second endpoint to x (delta(2,3)).
     ShAttrib4f delta = x(0,1,0,1) - L;
 
-    // compute unit tangent and normal 
+    // compute tangent d and normal d 
     ShAttrib4f dn = L(2,3,1,2) - L(0,1,3,0);
-    ShAttrib3f n_len;
-    n_len(0) = (dn(2,3)|dn(2,3));
-    n_len(1) = sqrt(n_len(0));
-    n_len(2) = ShAttrib1f(1.0)/n_len(1);
-    dn(2,3) = n_len(2,2) * dn(2,3);  // unit normal, now
-
-    ShAttrib3f r;  // return value
+    // make normal unit length, store as result gradient
+    r(2,3) = normalize(dn(2,3));
 
     // compute distance to line by projecting delta(0,1) onto normal
-    r(2) = (dn(2,3)|delta(0,1));  // signed distance from line
-    r(0) = r(2)*r(2);             // squared distance from line
-    r(1) = abs(r(2));             // unsigned distance from line
+    r(1) = (r(2,3)|delta(0,1));  // signed distance from line
+    // fix signs to get unsigned distance and its gradient
+    r(0,2,3) = cond(r(1) > 0.0,r(1,2,3),-r(1,2,3)); 
 
     // compute dot products of d with delta vectors
-    ShAttrib4f c;
-    c = dn(0,1,0,1) * delta;
-    c(0,1) = c(0,2) + c(1,3);
+    ShAttrib4f c = dn(0,1,0,1) * delta;
+    c(0,1) = c(0,2) + c(1,3);  // c(0) = d|delta(0,1), c(1) = d|delta(2,3)
 
-    // compute distance to endpoints
-    delta = delta * delta;  // square all components
-    delta(0,1) = delta(0,2) + delta(1,3);  // squared lengths
-    delta(0) = min(delta(0),delta(1));     // minimum squared length
-    delta(1) = sqrt(delta(0));             // unsigned length
+    // compute lengths of delta vectors
+    ShAttrib4f s = delta * delta;  // square all components
+    s(1,2) = s(0,2) + s(1,3);      // squared lengths
+
+    // pick smaller lengths and vectors
+    s(0) = sqrt(min(s(1),s(2))); // smaller length
+    s(1,2) = cond(s(1) < s(2),delta(0,1),delta(2,3)); // appropriate gradient
+    s(1,2) = s(1,2) * (ShAttrib1f(1.0)/s(0)); // normalize 
 
     // replace distances if beyond ends of line
-    r(0,1) = cond(c(0) > 0.0,r(0,1),delta(0,1));
-    r(0,1) = cond(c(1) < 0.0,r(0,1),delta(0,1));
+    r(0,2,3) = cond(c(0) > 0.0,r(0,2,3),s(0,1,2));
+    r(0,2,3) = cond(c(1) < 0.0,r(0,2,3),s(0,1,2));
 
     return r;
 }
@@ -108,20 +110,26 @@ segdist (
   static ShAttrib1f m_scale;
   static ShVector2f m_offset;
   static ShAttrib1f m_fw;
+  static ShAttrib2f m_thres;
   static ShColor3f m_color1, m_color2;
   static ShColor3f m_vcolor1, m_vcolor2;
 };
 
 VectorText::VectorText(int mode)
   : Shader(std::string("Vector Graphics: Vector Text") + 
-		  ((mode == 0) ? ": Antialiased" : 
-		   ((mode == 1) ? ": Aliased" : ": Distance"))),
+		  ((mode == 0) ? ": Isotropically Antialiased" : 
+		   ((mode == 1) ? ": Anisotropically Antialiased" : 
+		    ((mode == 3) ? ": Isotropically Antialiased Outline" : 
+		    ((mode == 4) ? ": Anisotropically Antialiased Outline" : 
+		     ((mode == 5) ? ": Gradient" : 
+		      ((mode == 6) ? ": Pseudodistance" : 
+		       ((mode == 2) ? ": Aliased" : ": Distance")))))))),
     m_mode(mode)
 {
   if (!m_done_init) {
     // Initialize parameters and static variables
     m_scale.name("scale");
-    m_scale.range(0.0, 5.0);
+    m_scale.range(0.0, 100.0);
 
     m_fw.name("filter width");
     m_fw.range(0.0, 10.0);
@@ -131,6 +139,9 @@ VectorText::VectorText(int mode)
 
     m_size.name("size");
     m_size.range(0.0, 100.0);
+
+    m_thres.name("threshold");
+    m_thres.range(-1.0, 1.0);
 
     m_color1.name("color1");
     m_color2.name("color2");
@@ -153,10 +164,10 @@ bool VectorText::init()
 
   // a test consisting of two contours (a letter A, actually)
   const int N = 11;
-  // const int N = 21;
+  // const int N = 22;
   ShAttrib4f L[N];
 
-  /* DOESN'T WORK! Ambiguity in sign at acute vertices.
+  /* DOESN'T WORK! Ambiguity in sign at acute vertices 
   L[0] = ShAttrib4f(0.3,0.0,0.0,0.0);
   L[1] = ShAttrib4f(0.0,0.0,0.5,1.5);
   L[2] = ShAttrib4f(0.5,1.5,0.9,1.5);
@@ -200,42 +211,93 @@ bool VectorText::init()
     ShAttrib2f x = (tc - m_offset) * m_size;
 
     // compute signed distance map
-    ShAttrib3f r = segdist(L[0],x);
-    ShAttrib3f nr;
+    ShAttrib4f r = segdist(L[0],x);
+    ShAttrib4f nr;
     for (int i=1; i<N; i++) {
        nr = segdist(L[i],x);
        r = cond(nr(0) < r(0),nr,r);
     }
-    r(0,1) = cond(r(2) < 0.0, -r(0,1), r(0,1));
+    // transfer sign
+    r(0) = cond(r(1) < 0.0, -r(0), r(0));
 
-    if (0 == m_mode) {
-        // antialiased rendering
+    switch (m_mode) {
+      case 0: {
+        // isotropically antialiased rendering
         ShAttrib2f fw = fwidth(x);
-	fw(0) = max(fw(0),fw(1))*m_fw;
-        ShAttrib1f p = smoothstep(-fw(0),fw(1),r(1));
+        ShAttrib1f w = max(fw(0),fw(1))*m_fw;
+        ShAttrib1f p = smoothstep(-w,w,r(0)+m_thres(0));
         o = lerp(p,m_color2,m_color1);
-    } else if (1 == m_mode) {
-	// aliased rendering
-        o = cond(r(2) >= 0.0,m_color2,m_color1);
-    } else {
-	// distance map visualization
-        o = (abs(r(1)) * m_scale)(0,0,0) * cond(r(1) >= 0.0,m_vcolor2,m_vcolor1);
+      } break;
+      case 1: {
+        // anisotropically antialiased rendering
+	ShAttrib2f fw;
+	fw(0) = dx(x) | r(2,3);
+	fw(1) = dy(x) | r(2,3);
+	ShAttrib1f w = length(fw);
+        ShAttrib1f p = smoothstep(-w,w,r(0)+m_thres(0));
+        o = lerp(p,m_color2,m_color1);
+      } break;
+      case 2: {
+        // aliased rendering
+        o = cond(r(0)+m_thres(0) > 0.0,m_color2,m_color1);
+      } break;
+      case 3: {
+        // isotropically antialiased outline rendering
+        ShAttrib2f fw = fwidth(x);
+        ShAttrib1f w = max(fw(0),fw(1))*m_fw;
+        ShAttrib2f p;
+	p(0) = smoothstep(-w,w,r(0)+m_thres(0));
+        p(1) = smoothstep(-w,w,-r(0)-m_thres(1));
+        o = lerp((1-p(0))*(1-p(1)),m_color1,m_color2);
+      } break;
+      case 4: {
+        // anisotropically antialiased outline rendering
+	ShAttrib2f fw;
+	fw(0) = dx(x) | r(2,3);
+	fw(1) = dy(x) | r(2,3);
+	ShAttrib1f w = length(fw);
+        ShAttrib2f p;
+	p(0) = smoothstep(-w,w,r(0)+m_thres(0));
+        p(1) = smoothstep(-w,w,-r(0)-m_thres(1));
+        o = lerp((1-p(0))*(1-p(1)),m_color1,m_color2);
+      } break;
+      case 5: {
+        // gradient visualization
+        o = 0.5 * (r(2) + 1.0) * m_vcolor1 
+	  + 0.5 * (r(3) + 1.0) * m_vcolor2;
+      } break;
+      case 6: {
+        // pseudodistance visualization
+        o = (abs(r(1)) * m_scale)(0,0,0) 
+          * cond(r(1) >= 0.0,m_vcolor2,m_vcolor1);
+      } break;
+      default: {
+        // distance map visualization
+        o = (abs(r(0)) * m_scale)(0,0,0) 
+          * cond(r(0) >= 0.0,m_vcolor2,m_vcolor1);
+      } break;
     }
   } SH_END_PROGRAM;
   return true;
 }
 
 bool VectorText::m_done_init = false;
-ShAttrib1f VectorText::m_scale = ShAttrib1f(1.0);
-ShVector2f VectorText::m_offset = ShVector2f(0.0,0.0);
-ShAttrib1f VectorText::m_size = ShAttrib1f(1.0);
+ShAttrib1f VectorText::m_scale = ShAttrib1f(6.0);
+ShVector2f VectorText::m_offset = ShVector2f(0.13,0.13);
+ShAttrib1f VectorText::m_size = ShAttrib1f(2.0);
 ShAttrib1f VectorText::m_fw = ShAttrib1f(1.0);
+ShAttrib2f VectorText::m_thres = ShAttrib2f(0.0,0.05);
 ShColor3f VectorText::m_color1 = ShColor3f(0.0, 0.0, 0.0);
 ShColor3f VectorText::m_color2 = ShColor3f(1.0, 1.0, 1.0);
-ShColor3f VectorText::m_vcolor1 = ShColor3f(1.0, 0.0, 0.0);
-ShColor3f VectorText::m_vcolor2 = ShColor3f(0.0, 1.0, 0.0);
+ShColor3f VectorText::m_vcolor1 = ShColor3f(1.0, 0.0, 1.0);
+ShColor3f VectorText::m_vcolor2 = ShColor3f(1.0, 1.0, 0.0);
 
-VectorText vt_with_aa = VectorText(0);
-VectorText vt_without_aa = VectorText(1);
-VectorText vt_distance = VectorText(2);
+VectorText vt_iaa = VectorText(0);
+VectorText vt_aaa = VectorText(1);
+VectorText vt_naa = VectorText(2);
+VectorText vt_iaa_outline = VectorText(3);
+VectorText vt_aaa_outline = VectorText(4);
+VectorText vt_grad = VectorText(5);
+// VectorText vt_pdistance = VectorText(6);
+VectorText vt_distance = VectorText(10);
 
