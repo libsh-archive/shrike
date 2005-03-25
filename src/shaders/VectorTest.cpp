2b08e512e52d989661758ba9027403a31f8b4156
zqin <zqin@afdca40c-03d6-0310-8ede-e9f093b21075>
2005-03-25 16:24:53 +0000

    added "test" version of vector test... really original version, that just has a hardcoded "A" character in parameters rather than using a texture map.
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/shrike/branches/vectortexture@2186 afdca40c-03d6-0310-8ede-e9f093b21075

--- src/shaders/VectorTest.cpp
+++ src/shaders/VectorTest.cpp
@@ -28,7 +28,6 @@
 #include <sh/shutil.hpp>
 #include <iostream>
 #include "Shader.hpp"
-#include "ShFont.hpp"
 #include "Globals.hpp"
 
 using namespace SH;
@@ -36,10 +35,10 @@ using namespace ShUtil;
 
 #include "util.hpp"
 
-class VectorText : public Shader {
+class VectorTest : public Shader {
 public:
-  VectorText(int mode);
-  ~VectorText();
+  VectorTest(int mode);
+  ~VectorTest();
 
   bool init();
 
@@ -64,7 +63,8 @@ private:
 ShAttrib4f //< true distance, signed value (dist from line), gradient vector
 segdist (
     ShAttrib4f L,  //< line segment (0,1 point 0, 2,3 point 1)
-    ShPoint2f  x   //< test point
+    ShPoint2f  x,  //< test point
+    bool norm_grad = true //< normalize gradient (by default, do)
 ) {
     // return value
     ShAttrib4f r;
@@ -94,7 +94,9 @@ segdist (
     // pick smaller lengths and vectors
     s(0) = sqrt(min(s(1),s(2))); // smaller length
     s(1,2) = cond(s(1) < s(2),delta(0,1),delta(2,3)); // appropriate gradient
-    s(1,2) = s(1,2) * (ShAttrib1f(1.0)/s(0)); // normalize 
+    if (norm_grad) {
+       s(1,2) = s(1,2) * (ShAttrib1f(1.0)/s(0)); // normalize 
+    }
 
     // replace distances if beyond ends of line
     r(0,2,3) = cond(c(0) > 0.0,r(0,2,3),s(0,1,2));
@@ -102,7 +104,6 @@ segdist (
 
     return r;
 }
-
 ShAttrib4f //< square distance, sign (plane equation), unnorm gradient vector
 segdist_t (
     ShAttrib4f L,  //< line segment (0,1 point 0, 2,3 point 1)
@@ -134,7 +135,6 @@ segdist_t (
 
     return r;
 }
-
 ShAttrib4f //< signed distance, signed value (dist from line), gradient vector
 segdists (
     ShAttrib4f L[],  //< line segments (0,1 point 0, 2,3 point 1)
@@ -164,14 +164,14 @@ segdists (
   static ShAttrib1f m_scale;
   static ShVector2f m_offset;
   static ShAttrib1f m_fw;
-
   static ShAttrib2f m_thres;
   static ShColor3f m_color1, m_color2;
   static ShColor3f m_vcolor1, m_vcolor2;
+  static ShAttrib2f m_celloffset, m_cellperiod;
 };
 
-VectorText::VectorText(int mode)
-  : Shader(std::string("Vector Graphics: Vector Text") + 
+VectorTest::VectorTest(int mode)
+  : Shader(std::string("Vector Graphics: Vector Test") + 
 	  ((mode == 0) ? ": Isotropically Antialiased" : 
 	  ((mode == 1) ? ": Anisotropically Antialiased" : 
 	  ((mode == 2) ? ": Aliased" : 
@@ -200,8 +200,14 @@ VectorText::VectorText(int mode)
     m_offset.name("offset");
     m_offset.range(-10.0, 10.0);
 
+    m_celloffset.name("cell offset");
+    m_celloffset.range(-1.0, 1.0);
+
+    m_cellperiod.name("cell period");
+    m_cellperiod.range(0.5, 10.0);
+
     m_size.name("size");
-    m_size.range(0.0, 1000.0);
+    m_size.range(0.0, 100.0);
 
     m_thres.name("threshold");
     m_thres.range(-1.0, 1.0);
@@ -215,93 +221,60 @@ VectorText::VectorText(int mode)
   }
 }
 
-VectorText::~VectorText()
+VectorTest::~VectorTest()
 {
 }
 
-bool VectorText::init()
+bool VectorTest::init()
 {
   std::cerr << "Initializing " << name() << std::endl;
   vsh = ShKernelLib::shVsh( Globals::mv, Globals::mvp );
   vsh = shSwizzle("texcoord", "posh") << vsh;
 
-  ShFont font;
-  font.loadFont("/home/mmccool/dev/vectortexture/freetype/d.txt");
+  // a test consisting of two contours (a letter A, actually)
+  const int N = 11;
+  // const int N = 22;
+  ShAttrib4f L[N];
 
-  int width = font.width();
-  int height = font.height();
-  int edges = font.edges();
-  float halfx = font.halfx();
-  float halfy = font.halfy();
-  int elements = 4;
+  // Data for a simple "A" character
+  L[0] = ShAttrib4f(0.3,0.0,0.0,0.0);
+  L[1] = ShAttrib4f(0.0,0.0,0.5,1.5);
+  L[2] = ShAttrib4f(0.5,1.5,0.9,1.5);
+  L[3] = ShAttrib4f(0.9,1.5,1.4,0.0);
+  L[4] = ShAttrib4f(1.4,0.0,1.1,0.0);
+  L[5] = ShAttrib4f(1.1,0.0,1.0,0.3);
+  L[6] = ShAttrib4f(1.0,0.3,0.4,0.3);
+  L[7] = ShAttrib4f(0.4,0.3,0.3,0);
+  L[8] = ShAttrib4f(0.5,0.6,0.9,0.6);
+  L[9] = ShAttrib4f(0.9,0.6,0.7,1.2);
+  L[10] = ShAttrib4f(0.7,1.2,0.5,0.6);
 
-  // textures for line segment endpoints
-  ShUnclamped< ShArrayRect<ShAttrib4f> > ftexture[edges];
-  for(int i=0; i<edges; i++) {
-	  ftexture[i].size(width, height);
-    ftexture[i].memory(font.memory(i));
+  // An Evil Hack: contract each line segment by epsilon.
+  // This resolves the ambiguity over which line segment is "closer"
+  // when a shared vertex is the closest point on a line segment, and
+  // allows the correct sign to be determined based only on the closest
+  // line segment.   By splitting the vertex by contracting both 
+  // vertices by the same amount, this gives a plane separating the
+  // two new vertices that is at the right angle for "sharp" miter rules
+  // when using the pseudodistance for outline and "Bold" versions of
+  // glyphs.
+  const float eps = 0.0001;
+  for (int i=0; i<N; i++) {
+    ShVector2f d = normalize(L[i](2,3) - L[i](0,1));
+    L[i](0,1) += d * eps;
+    L[i](2,3) -= d * eps;
   }
-
-  // texture for number of edges
-  // not used for now, just for debugging
-  ShArray2D<ShAttrib1f> findex(width, height);
-  findex.memory(font.edge());
-
-  // may not need this... but just in case
-  shUpdate();
-
-  /*
-  //debug info
-  for(int i=0; i<height; i++) {
-	  for(int j=0; j<width; j++) {
-		  int num = (int)font.edgenum()[i*width+j];
-		  std::cerr << (i * width + j) << " " << num << " " << std::endl;
-		  for(int l=0; l<edges; l++) {
-			  for(int m=0; m<elements; m++) {
-				  int index = (i * width + j) * elements + m;
-				  std::cout << font.coords(l)[index] << " ";
-			  }
-			  std::cout << std::endl;
-		  }
-	  }
-  }
-
-  //debug info
-  for(int i=0; i<height; i++) {
-	  for(int j=0; j<width; j++) {
-		  int num = (int)font.edgenum()[i*width+j];
-		  if(!num) { 
-		  	std::cerr << " " << " " << " ";
-			} else {
-		  	std::cerr << " " << "*" << " ";
-			}
-	  }
-	  std::cout << std::endl;
-  }
-  */
-
-  std::cerr << " the image width is " << font.width() << std::endl;
-  std::cerr << " the image height is " << font.height() << std::endl;
-  std::cerr << " the image maxedge is " << font.edges() << std::endl;
-  std::cerr << " the image halfx is " << font.halfx() << std::endl;
-  std::cerr << " the image halfy is " << font.halfy() << std::endl;
-
 
   fsh = SH_BEGIN_FRAGMENT_PROGRAM {
     ShInputTexCoord2f tc;
     ShOutputColor3f o;
 
     // transform texture coords (should be in vertex shader really, but)
+    ShAttrib2f x = (tc - m_offset) * m_size;
+    // x = (x - m_celloffset) % m_cellperiod; // tile plane 
 
-    //ShAttrib2f x = tc * m_size - m_offset;
-    ShAttrib2f x = tc - m_offset;
-    ShAttrib4f L[edges];
-
-    for(int i=0; i<edges; i++) {
-    	L[i] = ftexture[i](x); 
-    }
-  
-    ShAttrib4f r = segdists(L,edges,x);
+    // compute signed distance map, sign field, and gradient of distance map
+    ShAttrib4f r = segdists(L,N,x);
 
     switch (m_mode) {
       case 0: {
@@ -313,67 +286,67 @@ bool VectorText::init()
       } break;
       case 1: {
         // anisotropically antialiased rendering
-        ShAttrib2f fw;
-        fw(0) = dx(x) | r(2,3);
-        fw(1) = dy(x) | r(2,3);
-        ShAttrib1f w = length(fw)*m_fw;
+	ShAttrib2f fw;
+	fw(0) = dx(x) | r(2,3);
+	fw(1) = dy(x) | r(2,3);
+	ShAttrib1f w = length(fw)*m_fw;
         ShAttrib1f p = smoothstep(-w,w,r(0)+m_thres(0));
         o = lerp(p,m_color2,m_color1);
       } break;
       case 2: {
-        // aliased rendering;
+        // aliased rendering
         o = cond(r(0)+m_thres(0) > 0.0,m_color2,m_color1);
       } break;
       case 3: {
         // isotropically antialiased outline rendering
         ShAttrib2f fw = fwidth(x);
-        ShAttrib1f w = max(fw(0),fw(1))*m_fw;;
+        ShAttrib1f w = max(fw(0),fw(1))*m_fw;
         ShAttrib2f p;
-        p(0) = smoothstep(-w,w,r(0)+m_thres(0));
+	p(0) = smoothstep(-w,w,r(0)+m_thres(0));
         p(1) = smoothstep(-w,w,-r(0)-m_thres(1));
         o = lerp((1-p(0))*(1-p(1)),m_color1,m_color2);
       } break;
       case 4: {
-        // anisotropically antialiased outline rendering;
-        ShAttrib2f fw;;
-        fw(0) = dx(x) | r(2,3);
-        fw(1) = dy(x) | r(2,3);
-        ShAttrib1f w = length(fw)*m_fw;
+        // anisotropically antialiased outline rendering
+	ShAttrib2f fw;
+	fw(0) = dx(x) | r(2,3);
+	fw(1) = dy(x) | r(2,3);
+	ShAttrib1f w = length(fw)*m_fw;
         ShAttrib2f p;
-        p(0) = smoothstep(-w,w,r(0)+m_thres(0));
+	p(0) = smoothstep(-w,w,r(0)+m_thres(0));
         p(1) = smoothstep(-w,w,-r(0)-m_thres(1));
         o = lerp((1-p(0))*(1-p(1)),m_color1,m_color2);
       } break;
       case 5: {
         // gradient visualization
         o = 0.5 * (r(2) + 1.0) * m_vcolor1 
-          + 0.5 * (r(3) + 1.0) * m_vcolor2;;
+	  + 0.5 * (r(3) + 1.0) * m_vcolor2;
       } break;
       case 6: {
         // filter width visualization
-        ShAttrib2f fw = fwidth(x);	      
-        o = fw(0,1,0);
+	ShAttrib2f fw = fwidth(x);	      
+	o = fw(0,1,0);
       } break;
       case 7: {
         // isotropically antialiased pseudodistance outline rendering
-        ShAttrib2f fw = fwidth(x);;
+        ShAttrib2f fw = fwidth(x);
         ShAttrib1f w = max(fw(0),fw(1))*m_fw;
         ShAttrib2f p;
-        p(0) = smoothstep(-w,w,r(1)+m_thres(0));
-        p(1) = smoothstep(-w,w,-r(1)-m_thres(1));;
+	p(0) = smoothstep(-w,w,r(1)+m_thres(0));
+        p(1) = smoothstep(-w,w,-r(1)-m_thres(1));
         o = lerp((1-p(0))*(1-p(1)),m_color1,m_color2);
       } break;
       case 8: {
         // anisotropically antialiased pseudodistance outline rendering
-        ShAttrib2f fw;
-        fw(0) = dx(x) | r(2,3);
-        fw(1) = dy(x) | r(2,3);
-        ShAttrib1f w = length(fw)*m_fw;
+	ShAttrib2f fw;
+	fw(0) = dx(x) | r(2,3);
+	fw(1) = dy(x) | r(2,3);
+	ShAttrib1f w = length(fw)*m_fw;
         ShAttrib2f p;
-        p(0) = smoothstep(-w,w,r(1)+m_thres(0));
+	p(0) = smoothstep(-w,w,r(1)+m_thres(0));
         p(1) = smoothstep(-w,w,-r(1)-m_thres(1));
         o = lerp((1-p(0))*(1-p(1)),m_color1,m_color2);
-      } break;;
+      } break;
       case 9: {
         // biased signed distance map visualization 
         o = (0.5 + r(0) * m_scale)(0,0,0) 
@@ -407,30 +380,32 @@ bool VectorText::init()
   return true;
 }
 
-bool VectorText::m_done_init = false;
-ShAttrib1f VectorText::m_scale = ShAttrib1f(512);
-ShVector2f VectorText::m_offset = ShVector2f(0.000976562,0.000976562);
-ShAttrib1f VectorText::m_size = ShAttrib1f(512);
-ShAttrib1f VectorText::m_fw = ShAttrib1f(1.0);
-ShAttrib2f VectorText::m_thres = ShAttrib2f(0.0,0.05);
-ShColor3f VectorText::m_color1 = ShColor3f(0.0, 0.0, 0.0);
-ShColor3f VectorText::m_color2 = ShColor3f(1.0, 1.0, 1.0);
-ShColor3f VectorText::m_vcolor1 = ShColor3f(1.0, 0.0, 1.0);
-ShColor3f VectorText::m_vcolor2 = ShColor3f(1.0, 1.0, 0.0);
+bool VectorTest::m_done_init = false;
+ShAttrib1f VectorTest::m_scale = ShAttrib1f(6.0);
+ShVector2f VectorTest::m_offset = ShVector2f(0.13,0.13);
+ShAttrib2f VectorTest::m_celloffset = ShAttrib2f(0.2,0.2);
+ShAttrib2f VectorTest::m_cellperiod = ShAttrib2f(2.0,2.0);
+ShAttrib1f VectorTest::m_size = ShAttrib1f(2.0);
+ShAttrib1f VectorTest::m_fw = ShAttrib1f(1.0);
+ShAttrib2f VectorTest::m_thres = ShAttrib2f(0.0,0.05);
+ShColor3f VectorTest::m_color1 = ShColor3f(0.0, 0.0, 0.0);
+ShColor3f VectorTest::m_color2 = ShColor3f(1.0, 1.0, 1.0);
+ShColor3f VectorTest::m_vcolor1 = ShColor3f(1.0, 0.0, 1.0);
+ShColor3f VectorTest::m_vcolor2 = ShColor3f(1.0, 1.0, 0.0);
 
-VectorText vt_iaa = VectorText(0);
-VectorText vt_aaa = VectorText(1);
-VectorText vt_naa = VectorText(2);
-VectorText vt_iaa_outline = VectorText(3);
-VectorText vt_aaa_outline = VectorText(4);
-VectorText vt_grad = VectorText(5);
-VectorText vt_fw = VectorText(6);
-VectorText vt_pd_iaa_outline = VectorText(7);
-VectorText vt_pd_aaa_outline = VectorText(8);
-VectorText vt_biased_distance = VectorText(9);
-VectorText vt_grey_biased_distance = VectorText(10);
-VectorText vt_distance = VectorText(11);
-VectorText vt_biased_pdistance = VectorText(12);
-VectorText vt_grey_biased_pdistance = VectorText(13);
-VectorText vt_pdistance = VectorText(14);
+VectorTest vtest_iaa = VectorTest(0);
+VectorTest vtest_aaa = VectorTest(1);
+VectorTest vtest_naa = VectorTest(2);
+VectorTest vtest_iaa_outline = VectorTest(3);
+VectorTest vtest_aaa_outline = VectorTest(4);
+VectorTest vtest_grad = VectorTest(5);
+VectorTest vtest_fw = VectorTest(6);
+VectorTest vtest_pd_iaa_outline = VectorTest(7);
+VectorTest vtest_pd_aaa_outline = VectorTest(8);
+VectorTest vtest_biased_distance = VectorTest(9);
+VectorTest vtest_grey_biased_distance = VectorTest(10);
+VectorTest vtest_distance = VectorTest(11);
+VectorTest vtest_biased_pdistance = VectorTest(12);
+VectorTest vtest_grey_biased_pdistance = VectorTest(13);
+VectorTest vtest_pdistance = VectorTest(14);
 
