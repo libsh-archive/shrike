34b07a5904e85ec62c54312889e415ddee85abd2
francois <francois@afdca40c-03d6-0310-8ede-e9f093b21075>
2005-06-06 22:47:26 +0000

    Assign copyright to Serious Hack
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/shrike/trunk@2483 afdca40c-03d6-0310-8ede-e9f093b21075

--- src/shaders/Textures.cpp
+++ src/shaders/Textures.cpp
@@ -1,9 +1,6 @@
 // Sh: A GPU metaprogramming language.
 //
-// Copyright (c) 2003 University of Waterloo Computer Graphics Laboratory
-// Project administrator: Michael D. McCool
-// Authors: Zheng Qin, Stefanus Du Toit, Kevin Moule, Tiberiu S. Popa,
-//          Michael D. McCool
+// Copyright 2003-2005 Serious Hack Inc.
 // 
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
